f37d14548e5041551875581fac5d0668ab23a213
BombasticBomb <ahmadfarzadtaquee@gmail.com>
2025-07-23 13:45:12 -0500

    Update gravity_v3.py

--- gravity_v3.py
+++ gravity_v3.py
@@ -8,12 +8,14 @@ window.color = color.black
 
 # —————————————————————————————
 # Simulation parameters
-GM = 3000.5  # G·M term
+GM = 3000.5  # gravity
 start_radius = Vec3(-60, 40, 5)  # initial distance from sun
-initial_speed = 5.5  # launch speed
+initial_speed = 5.5  # velocity
 time_scale = 2  # simulation speed
 
 # —————————————————————————————
+
+
 # Individual UI Texts (all parented to camera.ui)
 state_txt = Text('', parent=camera.ui, position=(-0.52, 0.5), origin=(0, 1), scale=1.1, color=color.cyan)
 
@@ -33,7 +35,7 @@ planet.velocity = Vec3(0, 0, initial_speed)
 
 def update():
     global GM, start_radius, initial_speed, time_scale
-    # rotate for visual effect
+    # Rotation
     sun.rotation_y += time.dt * 5
     planet.rotation_y += time.dt * 15
 
@@ -59,7 +61,7 @@ def update():
     spd_txt.text = f'init_speed = {initial_speed}'
     sim_txt.text = f'sim_speed = {time_scale}'
     
-    # GM Control
+    # Gravity Control
     if held_keys['g'] and held_keys['up arrow']:
         GM += 10
     elif held_keys['g'] and held_keys['down arrow']:
@@ -71,16 +73,16 @@ def update():
     elif held_keys['s'] and held_keys['down arrow']:
         time_scale -= 0.1
 
-    # Initial Speed Control
+    # Velocity Control
     if held_keys['i'] and held_keys['up arrow']:
         planet.velocity = planet.velocity.normalized() * (planet.velocity.length() + 0.1)
     if held_keys['i'] and held_keys['down arrow']:
         planet.velocity = planet.velocity.normalized() * max(0, planet.velocity.length() - 0.1)
 
-    # Restart
+    # Restart the simulation by pressing Escape
     if held_keys['escape']:
         os.execv(sys.executable, ['python'] + sys.argv)
 
-# free camera
+
 EditorCamera()
 app.run()
